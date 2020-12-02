64a51a1ff1a617ef650ee98d38d3b368e2b1703c
rock-tran <rock.tran@manadr.com>
2020-12-02 17:44:54 +0700

    - Add import Binding class

--- src/main/kotlin/com/github/toantk238/sampleintellijplugin/actions/RenameKotlin.kt
+++ src/main/kotlin/com/github/toantk238/sampleintellijplugin/actions/RenameKotlin.kt
@@ -1,13 +1,15 @@
 package com.github.toantk238.sampleintellijplugin.actions
 
+import com.github.toantk238.sampleintellijplugin.util.runWriteCommand
 import com.google.common.base.CaseFormat
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.resolve.ImportPath
 
 class RenameKotlin : AnAction() {
 
@@ -25,19 +27,14 @@ class RenameKotlin : AnAction() {
             return
         }
         val ktFile: KtFile = psiFile
+        val psiFactory = KtPsiFactory(project)
 
+        // Watch current caret element
         val pos = editor.caretModel.offset
         val temp = ktFile.findElementAt(pos)
 
-        try {
-            val node = ktFile.node
-        } catch (e: Exception) {
-            logger.info("ToanTK That ko Exception : ${e.message}")
-        }
-
-        val psiFactory = KtPsiFactory(project)
         val properties = PsiTreeUtil.findChildrenOfType(ktFile, KtProperty::class.java)
-        val initFragmentToolProperty = properties.first { it.name == "initFragmentTools" }
+        val initFragmentToolProperty = properties.first { it.name == "initFragmentTools" } ?: return
         val propertyText = initFragmentToolProperty.text
         val foundText = "R.layout."
         val index1 = propertyText.indexOf(foundText)
@@ -46,41 +43,32 @@ class RenameKotlin : AnAction() {
         val newString = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, layoutFileName)
         val bindingClassName = "${newString}Binding"
 
-        val layoutBlock = PsiTreeUtil.findChildOfType(initFragmentToolProperty, KtLambdaExpression::class.java)
-        val type = psiFactory.createExpression("${bindingClassName}::class.java")
-        WriteCommandAction.runWriteCommandAction(project) {
-            layoutBlock?.replace(type)
+        val importElement = PsiTreeUtil.findChildOfType(ktFile, KtImportDirective::class.java)
+        val bindingClassPath = "com.base.databinding.$bindingClassName"
+        val importPath = ImportPath(FqName(bindingClassPath), false, null)
+        val bindingImportElement = psiFactory.createImportDirective(importPath)
+        project.runWriteCommand {
+            importElement?.add(psiFactory.createNewLine())
+            importElement?.add(bindingImportElement)
         }
 
+        val layoutBlock = PsiTreeUtil.findChildOfType(initFragmentToolProperty, KtLambdaExpression::class.java)
+        val bindClassExpression = psiFactory.createExpression("${bindingClassName}::class.java")
+        project.runWriteCommand { layoutBlock?.replace(bindClassExpression) }
+
         ktFile.accept(classRecursiveVisitor {
-            val name = it.name
             val superTypes = it.getColon()?.nextSibling?.nextSibling
             if (superTypes is KtSuperTypeList) {
                 val comma = psiFactory.createComma()
-                val type = psiFactory.createSuperTypeEntry(bindingClassName)
+                val typeEntry = psiFactory.createSuperTypeEntry(bindingClassName)
                 val lastGenericType = superTypes.firstChild.firstChild.firstChild.firstChild.lastChild.children[1]
-                WriteCommandAction.runWriteCommandAction(project) {
+                project.runWriteCommand {
                     lastGenericType.add(comma)
-                    lastGenericType.add(type)
+                    lastGenericType.add(typeEntry)
                 }
             }
-            val generics = it.getColon()?.nextSibling?.nextSibling?.children?.getOrNull(0)
-//                    as KtSuperTypeCallEntry?)?.typeArgumentList?.arguments
-            if (generics is KtSuperTypeCallEntry) {
-                val a = psiFactory.createComma()
-//                WriteCommandAction.runWriteCommandAction(project) {
-//                    generics.add(a)
-//                }
-                val arguments = generics.typeArguments
-                logger.info("ToanTK got Field Declaration: $name")
-            }
-//            val query = ClassInheritorsSearch.search(it)
         })
 
-//        val project = e.project ?: return
-//        val tempFile = editingFile.toPsiFile(project)
-//        if (tempFile !is KtFile) return
-//        val ktFile = tempFile as KtFile
         logger.info("ToanTK got Kt File done")
     }
 }
---
+++ src/main/kotlin/com/github/toantk238/sampleintellijplugin/util/ProjectKt.kt
@@ -0,0 +1,10 @@
+package com.github.toantk238.sampleintellijplugin.util
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+
+fun Project.runWriteCommand(block: () -> Unit) {
+    WriteCommandAction.runWriteCommandAction(this) {
+        block.invoke()
+    }
+}
